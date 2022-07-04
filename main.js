f2bc35649d289d070d2ebf2cedd61b3999eafb43
João Vitor de Magalhães Mendes <98191397+iijao@users.noreply.github.com>
2022-07-04 13:06:31 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const green = document.querySelector('.green');
+const obstaculo = document.querySelector('.obstaculo');
+
+const jump = () => {
+    green.classList.add('jump');
+
+    setTimeout(() => {
+        green.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(()=>{
+    const obstaculoPosition = obstaculo.offsetLeft;
+    const greenPosition = +window.getComputedStyle(green).bottom.replace('px', '');
+
+    console.log(greenPosition)
+
+    if(obstaculoPosition <= 90 && obstaculoPosition > 0 && greenPosition < 80) {
+
+        obstaculo.style.animation = 'none'
+        obstaculo.style.left = `${obstaculoPosition}px`
+
+        green.style.animation = 'none'
+        green.style.bottom = `${greenPosition}px`
+
+        green.src = '/images/morte.png';
+        green.style.width = '88px'
+
+
+        clearInterval(loop);
+    }
+
+},10)
+
+document.addEventListener('keydown', jump); 
